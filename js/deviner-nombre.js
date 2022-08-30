0df87c2170195dfd3a7683ffe43f2509daf97fb6
rayan-broq <94693010+rayan-broq@users.noreply.github.com>
2022-08-30 15:36:50 +0200

    premier commit

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,6 +1,6 @@
 /**
 * Jeu du nombre mystère
-* @author  Steve Fallet
+* @author  Rayan Broquet
 * @version 0.1
 * @since   2018-09-09 (date de création)
 */
@@ -8,13 +8,14 @@
 // Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
 (function main() {
     "use strict";
-
     /**
      * Retourne un nombre entier aléatoire compris entre min et max
      * @param min
      * @param max
      * @returns {number}
      */
+
+
     function tireNombre(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     }
