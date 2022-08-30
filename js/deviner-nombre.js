1b60262aca6c687bbb4fb234704b084bc08cfae4
rayan-broq <94693010+rayan-broq@users.noreply.github.com>
2022-08-30 16:14:31 +0200

    fin exercice

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,13 +1,14 @@
 /**
-* Jeu du nombre mystère
-* @author  Rayan Broquet
-* @version 0.1
-* @since   2018-09-09 (date de création)
-*/
+ * Jeu du nombre mystère
+ * @author  Rayan Broquet
+ * @version 0.1
+ * @since   2018-09-09 (date de création)
+ */
 
 // Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
 (function main() {
-    "use strict";
+    'use strict';
+
     /**
      * Retourne un nombre entier aléatoire compris entre min et max
      * @param min
@@ -15,9 +16,32 @@
      * @returns {number}
      */
 
-
     function tireNombre(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     }
 
+    const MIN = 1;
+    const MAX = 100;
+
+    let nombreMystere = tireNombre(MIN, MAX);
+    let nombreEssaye = 0;
+    let nombreCorrecte = false;
+
+    do {
+        let nombreChoisi = Number(prompt('Veuillez choisir un nombre entre 1 et 100'));
+
+        if (isNaN(nombreChoisi) || nombreChoisi > 100 || nombreChoisi < 1) {
+            alert('Saisie invalide !');
+        } else {
+            nombreEssaye++;
+            if (nombreChoisi === nombreMystere) {
+                alert(`Vous avez trouvé en ${nombreEssaye} essaies`);
+                break;
+            } else if (nombreMystere > nombreChoisi) {
+                alert('C\'est plus');
+            } else {
+                alert('C\'est moins');
+            }
+        }
+    } while (!nombreCorrecte);
 }()); // main IIFE
